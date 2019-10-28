5a3117c71deb0df55d2c496cae936a7450d961d9
Davi Tabosa <davitabosa12@hotmail.com>
2019-10-28 14:20:30 -0300

    renaming NoiteReceiver to NoiteAction

---
+++ UFCReminder-EC/app/src/main/java/br/ufc/smd/ufcreminder/NoiteAction.java
@@ -0,0 +1,30 @@
+package br.ufc.smd.ufcreminder;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.gms.awareness.fence.FenceState;
+
+import java.util.List;
+
+import smd.ufc.br.easycontext.fence.FenceAction;
+
+public class NoiteAction implements FenceAction {
+    @Override
+    public void doOperation(Context context, FenceState state, Bundle data) {
+        if (state.getCurrentState() == FenceState.TRUE) {
+            List<Mensagem> mensagemList =
+                    AppDb.getInstance(context).mensagemDao().getLocalHorario(Mensagem.Local.PICI, Mensagem.Horario.NOITE);
+
+            if (mensagemList.isEmpty())
+                return;
+            else {
+                for (Mensagem m :
+                        mensagemList) {
+                    PushNotification.pushMensagem(context, m);
+                    AppDb.getInstance(context).mensagemDao().delete(m);
+                }
+            }
+        }
+    }
+}
